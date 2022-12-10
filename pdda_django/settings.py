fe724b03300d9183b481e9f82ac1a9e237657f82
haroldhalog <95028802+haroldhalog@users.noreply.github.com>
2022-12-10 18:54:09 +0800

    Update settings.py

--- pdda_django/settings.py
+++ pdda_django/settings.py
@@ -28,8 +28,9 @@ SECRET_KEY = 'django-insecure-v(!)y2fv58uges^ub#r9n!-oydq=2m%=tv!5b@-x&9lih9^+^t
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['https://*.railway.app']
+    if not DEBUG:
+CSRF_TRUSTED_ORIGINS = [‘https://web-production-3d90.up.railway.app/’]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

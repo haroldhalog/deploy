4fcda3ef1ee37044f9ab31ceb534c5ab8b7fc9f7
haroldhalog <95028802+haroldhalog@users.noreply.github.com>
2022-12-12 15:07:52 +0800

    Update settings.py

--- pdda_django/settings.py
+++ pdda_django/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
+TEMPLATE_DIRS = (
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [

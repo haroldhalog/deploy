1408b398adc409edda63a80ad69f8e5b2e5657b0
haroldhalog <95028802+haroldhalog@users.noreply.github.com>
2022-12-12 14:20:15 +0800

    Update settings.py

--- pdda_django/settings.py
+++ pdda_django/settings.py
@@ -31,7 +31,7 @@ TEMPLATE_DEBUG = True
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['fuscia-cannon-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://fuscia-cannon-production.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
